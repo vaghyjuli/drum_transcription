0a0ec39b8e935d7aac941b558733cee1ccfbf71c
Julia Vaghy <32615436+vaghyjuli@users.noreply.github.com>
2022-05-08 22:53:15 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ import matplotlib.pyplot as plt
 
 
 class Sample:
-    def __init__(self, _dir, _bpm, _midi_file, _wav_file, _instruments):
+    def __init__(self, _dir, _bpm, _midi_file, _wav_file, _instrument_codes):
         self.dir = _dir
         self.bpm = _bpm
         self.midi_file = _midi_file
         self.wav_file = _wav_file
-        self.instruments = _instruments
+        self.instrument_codes = _instrument_codes
         self.init_midi_labels()
 
     def __str__(self):
@@ -43,7 +43,7 @@ class Sample:
             if msg.type == 'note_on':
                 self.labels.add_onset(msg.note, ticks)
         #self.labels.print_onsets()
-        # the conversion below might be incorrect
+        # DO: the conversion below might be incorrect
         #self.labels.plot((mid.length/ticks) * (120/self.bpm))
 
 
@@ -76,16 +76,24 @@ class MIDILabels:
             self.idx = _idx
             self.onsets = []
 
-        def add_onset(self, onset):
-            self.onsets.append(onset)
+        def __str__(self):
+            return self.label
 
         def print_onsets(self):
-            print(self.label, self.onsets)
+            print(f"{self.label}: {self.onsets}")
+
+        def add_onset(self, onset):
+            self.onsets.append(onset)
 
         def plot(self, tick_duration):
             for onset in self.onsets:
                 plt.plot(onset*tick_duration, (self.idx+1)*(200), 'o', color=self.color)
                 # convert to seconds
+
+
+class NMFLabels:
+    def __init__(self):
+        pass
 
 
 def read_data(data_folder):
@@ -94,31 +102,38 @@ def read_data(data_folder):
     sample_directories = [item for item in os.listdir() if os.path.isdir(item)]
     for sample_directory in sample_directories:
         os.chdir(os.path.join(data_folder, sample_directory))
+        make_path = lambda f : os.path.join(data_folder, sample_directory, f)
         if len(glob.glob("*.mid")) != 1:
             print(f"There should be a single MIDI file in {sample_directory}")
             continue
-        midi_file = os.path.join(data_folder, sample_directory, glob.glob("*.mid")[0])
+        midi_file = make_path(glob.glob("*.mid")[0])
         if len(glob.glob("*.wav")) != 1:
             print(f"There should be a single WAV file in {sample_directory}")
             continue
-        wav_file = os.path.join(data_folder, sample_directory, glob.glob("*.wav")[0])
+        wav_file = make_path(glob.glob("*.wav")[0])
+        instrument_codes = {}
         with open ("info.txt", "r") as info_file:
             data = info_file.read().splitlines()
             n_instruments = len(data) - 4
             try:
                 bpm = int(data[0].split()[0])
-            except (ValueError, Exception) as error:
+            except Exception as error:
                 print(f"Failed to identify BPM in {sample_directory}")
                 continue
-        os.chdir(os.path.join(data_folder, sample_directory, "instruments"))
-        if os.listdir() != glob.glob("*.wav"):
-            print(f"Strange files in {sample_directory}/instruments")
+            try:
+                for i in range(4, len(data)):
+                    instrument_codes[data[i].split()[0]] = data[i].split()[1]
+            except Exception as error:
+                print(f"Failed to identify instrument codes in {sample_directory}")
+                continue
+        os.chdir(make_path("instruments"))
+        missing_instruments = [i for i in instrument_codes.values() if i not in glob.glob("*.wav")]
+        if len(missing_instruments) > 0:
+            print(f"{missing_instruments} missing in {sample_directory}/instruments")
             continue
-        if len(glob.glob("*.wav")) != n_instruments:
-            print(f"According to info.txt, there should be {n_instruments} WAV files in {sample_directory}/instruments")
-            continue
-        instruments = [os.path.join(data_folder, sample_directory, recording) for recording in glob.glob("*.wav")]
-        samples.append(Sample(sample_directory, bpm, midi_file, wav_file, instruments))
+        for midi_code, instrument_wav in instrument_codes.items():
+            instrument_codes[midi_code] = make_path(instrument_wav)
+        samples.append(Sample(sample_directory, bpm, midi_file, wav_file, instrument_codes))
     return samples
 
 
