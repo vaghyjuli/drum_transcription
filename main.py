a2e98c01b711ed8aa785240fce62f105b2dfadc4
Júlia Vághy <32615436+vaghyjuli@users.noreply.github.com>
2022-05-10 19:43:40 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -33,12 +33,14 @@ class Sample:
     """
     def __init__(self, _dir, _bpm, _midi_file, _wav_file, _instrument_codes):
         self.dir = _dir
-        self.midi_file = _midi_file
-        self.wav_file = _wav_file
         self.instrument_codes = _instrument_codes
         self.midi_labels = MIDILabels(_midi_file, _bpm, _instrument_codes)
         #self.midi_labels.print_onsets()
         #self.midi_labels.plot()
+        self.nmf_labels = NMFLabels(_wav_file, _instrument_codes)
+        #self.nmf_labels.plot_instrument_spectra()
+        #self.nmf_labels.plot_template_matrix()
+        #self.nmf_labels.plot_recording_spectrum()
 
     def __str__(self):
         return self.dir
@@ -100,7 +102,6 @@ class MIDILabels:
         # DO: the conversion below might be incorrect
         self.tick_duration = (mid.length/ticks) * (120/self.bpm)
 
-    # rewrite to print_midi_onsets()
     def print_onsets(self):
         """
             Prints the onset ticks for each instrument.
@@ -109,14 +110,13 @@ class MIDILabels:
         for midi_note, instrument in self.instrument_codes.items():
             instrument.print_onsets()
 
-    # rewrite to plot_midi()
     def plot(self):
         """
             Visualizes the MIDI file's onsets in a plot.
         """
         fig, ax = plt.subplots(1)
         for midi_note, instrument in self.instrument_codes.items():
-            instrument.plot(self.tick_duration)
+            instrument.plot_midi(self.tick_duration)
         ax.set_yticklabels([])
         plt.xlabel('Time (s)')
         plt.show()
@@ -124,9 +124,96 @@ class MIDILabels:
 
 class NMFLabels:
     def __init__(self, _wav_file, _instrument_codes):
-        self.wav_file = wav_file
-        pass
+        self.wav_file = _wav_file
+        self.instrument_codes = _instrument_codes
+        self.N = 512
+        self.H = 256
+        self.initialize_template_matrix()
+        self.calculate_STFT()
+        self.nmf(init=True)
+        #self.plot_template_matrix()
+        #self.plot_activation_matrix()
 
+    def initialize_template_matrix(self):
+        templates = [instrument.template for midi_note, instrument in self.instrument_codes.items()]
+        self.W_init = np.array(templates, dtype=np.float64).transpose()
+
+    def calculate_STFT(self):
+        x, self.Fs = librosa.load(self.wav_file)
+        X = librosa.stft(x, n_fft=self.N, hop_length=self.H, win_length=self.N, window='hann', center=True, pad_mode='constant')
+        self.V = np.log(1 + 10 * np.abs(X))
+
+    def plot_template_matrix(self):
+        fig = plt.figure(figsize=(4, 8))
+        ax = fig.add_subplot(111)
+        ax.imshow(self.W_init, cmap='gray', vmin=0, origin='lower', interpolation='nearest')
+        plt.title('W_init')
+        plt.show()
+
+    def plot_activation_matrix(self):
+        fig = plt.figure(figsize=(10, 8))
+        ax = fig.add_subplot(111)
+        ax.imshow(self.H, cmap='gray', vmin=0, origin='lower', interpolation='nearest')
+        plt.title('H')
+        plt.show()
+
+    def plot_instrument_spectra(self):
+        for midi_note, instrument in self.instrument_codes.items():
+            instrument.plot_recording()
+
+    def plot_recording_spectrum(self):
+        T_coef = np.arange(self.Y.shape[1]) * self.H / self.Fs
+        F_coef = np.arange(self.Y.shape[0]) * self.Fs / self.N
+        left = min(T_coef)
+        right = max(T_coef) + self.N / self.Fs
+        lower = min(F_coef)
+        upper = max(F_coef)
+        plt.imshow(self.Y, vmin=0, origin='lower', aspect='auto', cmap='gray_r', extent=[left, right, lower, upper])
+        plt.xlabel('Time (seconds)')
+        plt.ylabel('Frequency (Hz)')
+        plt.title('Spectrogram')
+        plt.show()
+
+    def nmf(self, init=False, norm=False, report=False):
+        thresh=0.001
+        L=1000
+        R = len(self.instrument_codes)
+        K = self.V.shape[0]
+        N = self.V.shape[1]
+        W = self.W_init if init else np.random.rand(K, R)
+        H = np.random.rand(R, N)
+        H_W_error = np.zeros((2, L))
+        ell = 1
+        below_thresh = False
+        eps_machine = np.finfo(np.float32).eps
+        while not below_thresh and ell <= L:
+            H_ell = H
+            W_ell = W
+            H = H * (W.transpose().dot(self.V) / (W.transpose().dot(W).dot(H) + eps_machine))
+            W = W * (self.V.dot(H.transpose()) / (W.dot(H).dot(H.transpose()) + eps_machine))
+            H_error = np.linalg.norm(H - H_ell, ord=2)
+            W_error = np.linalg.norm(W - W_ell, ord=2)
+            H_W_error[:, ell-1] = [H_error, W_error]
+            if report:
+                print('Iteration: ', ell, ', H_error: ', H_error, ', W_error: ', W_error)
+            if H_error < thresh and W_error < thresh:
+                below_thresh = True
+                H_W_error = H_W_error[:, 0:ell]
+            ell += 1
+        if norm:
+            for r in range(R):
+                v_max = np.max(W[:, r])
+                if v_max > 0:
+                    W[:, r] = W[:, r] / v_max
+                    H[r, :] = H[r, :] * v_max
+        self.V_approx = W.dot(H)
+        V_approx_err = np.linalg.norm(self.V - self.V_approx, ord=2)
+        self.W = W
+        self.H = H
+        print(f"NMF finished with V_approx_err={V_approx_err}")
+
+    def find_onsets(self):
+        pass
 
 class Instrument:
     """
@@ -160,7 +247,6 @@ class Instrument:
         self.midi_onsets = [] # tick
         self.wav_file = _wav_file
         self.N = 512
-        # TODO: fix N! - change in NMF algo
         self.H = 256
         self.init_template()
 
@@ -180,15 +266,23 @@ class Instrument:
     def init_template(self):
         x, self.Fs = librosa.load(self.wav_file)
         X = librosa.stft(x, n_fft=self.N, hop_length=self.H, win_length=self.N, window='hann', center=True, pad_mode='constant')
-        self.Y = np.log(1 + 10 * np.abs(X)) # self.Y.shape = (1025, T)
-        #self.Y = self.Y[0:100,:] # self.Y.shape = (100, T)
-        ## TODO: OPTIMIZE normalization
-        #self.Y = normalize_feature_sequence(X=self.Y, norm="z")
-        #print(self.Y.shape)
+        self.Y = np.log(1 + 10 * np.abs(X))
         self.template = np.mean(self.Y, axis=1)
-        #print(self.template)
 
-    def plot(self, tick_duration):
+    def plot_recording(self):
+        T_coef = np.arange(self.Y.shape[1]) * self.H / self.Fs
+        F_coef = np.arange(self.Y.shape[0]) * self.Fs / self.N
+        left = min(T_coef)
+        right = max(T_coef) + self.N / self.Fs
+        lower = min(F_coef)
+        upper = max(F_coef)
+        plt.imshow(self.Y, vmin=0, origin='lower', aspect='auto', cmap='gray_r', extent=[left, right, lower, upper])
+        plt.xlabel('Time (seconds)')
+        plt.ylabel('Frequency (Hz)')
+        plt.title('Spectrogram')
+        plt.show()
+
+    def plot_midi(self, tick_duration):
         for onset in self.midi_onsets:
             plt.plot(onset*tick_duration, (self.idx+1)*(200), 'o', color=self.color)
             # convert to seconds
