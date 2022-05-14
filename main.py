9d0f62f003d3c0038140a570a9f37c141231e89e
Júlia Vághy <32615436+vaghyjuli@users.noreply.github.com>
2022-05-14 19:02:41 +0200

    nmfd v0

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 import librosa
 import numpy as np
 from scipy import signal
+from copy import deepcopy
 
 class Sample:
     """
@@ -160,11 +161,12 @@ class NMFLabels:
     def initialize_template_matrix(self):
         if self.nmf_type == 'NMF':
             templates = [instrument.template for midi_note, instrument in self.instrument_codes.items()]
+            self.W_init = np.array(templates, dtype=np.float64).transpose()
         elif self.nmf_type == 'NMFD':
             T = max([instrument.Y.shape[1] for midi_note, instrument in self.instrument_codes.items()])
             templates = [instrument.template_2D(T) for midi_note, instrument in self.instrument_codes.items()]
-        self.W_init = np.array(templates, dtype=np.float64).transpose()
-    
+            self.W_init = np.array(templates, dtype=np.float64).transpose((1, 0, 2))
+
     def factorize(self):
         if self.nmf_type == 'NMF':
             self.nmf(init=True)
@@ -252,11 +254,16 @@ class NMFLabels:
 
     def nmfd(self):
         L = 1000
-        R = len(self.instrument_codes)
+        R = self.W_init.shape[1]
         K = self.V.shape[0]
         N = self.V.shape[1]
-        T = self.W_init.shape[0]
-        print(T)
+        T = self.W_init.shape[2]
+        W, H, nmfdV, costFunc, tensorW = NMFD(V=self.V, T=T, R=R, W_init=self.W_init, fixW=True)
+        i = 0
+        for midi_note, instrument in self.instrument_codes.items():
+            instrument.set_activation(H[i])
+            instrument.find_onsets(plot=True)
+            i+=1
 
 class Instrument:
     """
@@ -375,7 +382,7 @@ class Instrument:
         T_coef = np.arange(len(self.activations)) / Fs_feature
         novelty = half_wave(np.append([0], np.diff(self.activations)))
         enhanced_novelty = half_wave(novelty - self.local_avg(novelty))
-        peaks, properties = signal.find_peaks(enhanced_novelty, prominence=0.5)
+        peaks, properties = signal.find_peaks(enhanced_novelty, prominence=0.3)
         self.nmf_onsets = T_coef[peaks]
 
         if plot:
@@ -404,7 +411,7 @@ class Instrument:
                 self.fn_count += 1
         if nmf_idx < len(self.nmf_onsets)-1:
             self.fp_count += len(self.nmf_onsets) - nmf_idx - 1
-        #print(f"{self.wav_file}\nTP={self.tp_count}\nFP={self.fp_count}\nFN={self.fn_count}\n")
+        print(f"{self.wav_file}\nTP={self.tp_count}\nFP={self.fp_count}\nFN={self.fn_count}\n")
 
     def local_avg(self, arr):
         avg_window = 3
@@ -504,6 +511,201 @@ def read_data(data_folder):
             continue
     return samples
 
+EPS = 2.0 ** -52
+
+def NMFD(V, T, R, W_init, L=10, fixW=False):
+    """
+        Non-Negative Matrix Factor Deconvolution with Kullback-Leibler-Divergence
+        and fixable components.
+
+        Parameters
+        ----------
+        V: array-like
+            Matrix that shall be decomposed (typically a magnitude spectrogram of dimension
+            numBins x numFrames)
+
+        L: int
+            Number of NMFD iterations
+
+        T: int
+            Number of time frames for 2D-templates
+
+        R: int
+            Number of NMF components        
+
+        Returns
+        -------
+        W: array-like
+            List with the learned templates
+
+        H: array-like
+            Matrix with the learned activations
+
+        nmfdV: array-like
+            List with approximated component spectrograms
+
+        costFunc: array-like
+            The approximation quality per iteration
+
+        tensorW: array-like
+            If desired, we can also return the tensor
+    """
+    # use parameter nomenclature as in [2]
+    K, N = V.shape
+    initH = np.random.rand(R, N)
+    tensorW = np.zeros((K, R, T))
+    costFunc = np.zeros(L)
+
+    # stack the templates into a tensor
+    for r in range(R):
+        tensorW[:, r, :] = W_init[:, r, :]
+
+    # the activations are matrix shaped
+    H = deepcopy(initH)
+
+    # create helper matrix of all ones (denoted as J in eq (5,6) in [2])
+    onesMatrix = np.ones((K, N))
+
+    # this is important to prevent initial jumps in the divergence measure
+    V_tmp = V / (EPS + V.sum())
+
+    for iteration in range(L):
+        # compute first approximation
+        Lambda = convModel(tensorW, H)
+
+        # store the divergence with respect to the target spectrogram
+        costMat = V_tmp * np.log(1.0 + V_tmp/(Lambda+EPS)) - V_tmp + Lambda
+        costFunc[iteration] = costMat.mean()
+
+        # compute the ratio of the input to the model
+        Q = V_tmp / (Lambda + EPS)
+
+        # accumulate activation updates here
+        multH = np.zeros((R, N))
+
+        # go through all template frames
+        for t in range(T):
+            # use tau for shifting and t for indexing
+            tau = deepcopy(t)
+
+            # The update rule for W as given in eq. (5) in [2]
+            # pre-compute intermediate, shifted and transposed activation matrix
+            transpH = shiftOperator(H, tau).T
+
+            # multiplicative update for W
+            multW = Q @ transpH / (onesMatrix @ transpH + EPS)
+
+            if not fixW:
+                tensorW[:, :, t] *= multW
+
+            # The update rule for W as given in eq. (6) in [2]
+            # pre-compute intermediate matrix for basis functions W
+            transpW = tensorW[:, :, t].T
+
+            # compute update term for this tau
+            addW = (transpW @ shiftOperator(Q, -tau)) / (transpW @ onesMatrix + EPS)
+
+            # accumulate update term
+            multH += addW
+
+        # multiplicative update for H, with the average over all T template frames
+        H *= multH / T
+
+        # normalize templates to unit sum
+        normVec = tensorW.sum(axis=2).sum(axis=0)
+
+        tensorW *= 1.0 / (EPS+np.expand_dims(normVec, axis=1))
+
+    W = list()
+    nmfdV = list()
+
+    # compute final output approximation
+    for r in range(R):
+        W.append(tensorW[:, r, :])
+        nmfdV.append(convModel(np.expand_dims(tensorW[:, r, :], axis=1), np.expand_dims(H[r, :], axis=0)))
+
+    return W, H, nmfdV, costFunc, tensorW
+
+
+def convModel(W, H):
+    """
+        Convolutive NMF model implementing the eq. (4) from [2]. Note that it can
+        also be used to compute the standard NMF model in case the number of time
+        frames of the templates equals one.
+
+        Parameters
+        ----------
+        W: array-like
+            Tensor holding the spectral templates which can be interpreted as a set of
+            spectrogram snippets with dimensions: numBins x numComp x numTemplateFrames
+
+        H: array-like
+            Corresponding activations with dimensions: numComponents x numTargetFrames
+
+        Returns
+        -------
+        lamb: array-like
+            Approximated spectrogram matrix
+    """
+    # the more explicit matrix multiplication will be used
+    K, R, T = W.shape
+    R, N = H.shape
+
+    # initialize with zeros
+    lamb = np.zeros((K, N))
+
+    # this is doing the math as described in [2], eq (4)
+    # the alternative conv2() method does not show speed advantages
+
+    for k in range(T):
+        multResult = W[:, :, k] @ shiftOperator(H, k)
+        lamb += multResult
+
+    lamb += EPS
+
+    return lamb
+
+
+def shiftOperator(A, shiftAmount):
+    """
+        Shift operator as described in eq. (5) from [2]. It shifts the columns
+        of a matrix to the left or the right and fills undefined elements with
+        zeros.
+
+        Parameters
+        ----------
+        A: array-like
+            Arbitrary matrix to undergo the shifting operation
+
+        shiftAmount: int
+            Positive numbers shift to the right, negative numbers
+            shift to the left, zero leaves the matrix unchanged
+
+        Returns
+        -------
+        shifted: array-like
+            Result of this operation
+    """
+    # Get dimensions
+    numRows, numCols = A.shape
+
+    # Limit shift range
+    shiftAmount = np.sign(shiftAmount) * min(abs(shiftAmount), numCols)
+
+    # Apply circular shift along the column dimension
+    shifted = np.roll(A, shiftAmount, axis=-1)
+
+    if shiftAmount < 0:
+        shifted[:, numCols + shiftAmount: numCols] = 0
+
+    elif shiftAmount > 0:
+        shifted[:, 0: shiftAmount] = 0
+
+    else:
+        pass
+
+    return shifted
+
 data_folder = r'/Users/juliavaghy/Desktop/0-syth_data/data'
 samples = read_data(data_folder)
 print(f"\nIncluded samples: {samples}\n")
@@ -515,9 +717,15 @@ for sample in samples:
     tp_count += tp_sample
     fp_count += fp_sample
     fn_count += fn_sample
-precision = tp_count / (tp_count + fp_count)
-recall = tp_count / (tp_count + fn_count)
-f_measure = (2*tp_count) / (2*tp_count + fp_count + fn_count)
+
+if tp_count == 0:
+    precision = 0
+    recall = 0
+    f_measure = 0
+else:
+    precision = tp_count / (tp_count + fp_count)
+    recall = tp_count / (tp_count + fn_count)
+    f_measure = (2*tp_count) / (2*tp_count + fp_count + fn_count)
 print(f"TP={tp_count}, FP={fp_count}, FN={fn_count}")
 print(f"precision = {precision}")
 print(f"recall = {recall}")
