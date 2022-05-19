424ef5665c3e50862e83eb2b75580323b9cd1f76
Júlia Vághy <32615436+vaghyjuli@users.noreply.github.com>
2022-05-19 21:36:31 +0200

    simplified

--- drum_transcription/nmf.py
+++ drum_transcription/nmf.py
@@ -1,35 +1,24 @@
 import numpy as np
 
-def NMF(V, W_init, fixW=True, norm=False, thresh = 0.001, L = 1000, report=False):
-    K, R = W_init.shape
+def NMF(V, W, fixW=True, thresh = 0.001, L = 1000):
+    K, R = W.shape
     K, N = V.shape
-    W = W_init
     H = np.random.rand(R, N)
-    H_W_error = np.zeros((2, L))
-    ell = 1
-    below_thresh = False
+
     eps_machine = np.finfo(np.float32).eps
-    while not below_thresh and ell <= L:
-        H_ell = H
-        W_ell = W
+
+    W_diff = float('inf')
+    for _ in range(L):
+        H_prev = H
         H = H * (W.transpose().dot(V) / (W.transpose().dot(W).dot(H) + eps_machine))
+        H_diff = np.linalg.norm(H - H_prev, ord=2)
         if not fixW:
+            W_prev = W
             W = W * (V.dot(H.transpose()) / (W.dot(H).dot(H.transpose()) + eps_machine))
-        H_error = np.linalg.norm(H - H_ell, ord=2)
-        W_error = np.linalg.norm(W - W_ell, ord=2)
-        H_W_error[:, ell-1] = [H_error, W_error]
-        if report:
-            print('Iteration: ', ell, ', H_error: ', H_error, ', W_error: ', W_error)
-        if H_error < thresh and W_error < thresh:
-            below_thresh = True
-            H_W_error = H_W_error[:, 0:ell]
-        ell += 1
-    if norm:
-        for r in range(R):
-            v_max = np.max(W[:, r])
-            if v_max > 0:
-                W[:, r] = W[:, r] / v_max
-                H[r, :] = H[r, :] * v_max
+            W_diff = np.linalg.norm(W - W_prev, ord=2)
+        if H_diff < thresh and W_diff < thresh:
+            break
+
     V_approx = W.dot(H)
     V_approx_err = np.linalg.norm(V - V_approx, ord=2)
     #print(f"NMF finished with V_approx_err={V_approx_err}\n")
