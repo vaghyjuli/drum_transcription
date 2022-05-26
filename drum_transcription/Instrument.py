fixed self-sim normalization

drum_transcription/Instrument.py
@@ -64,25 +64,33 @@ class Instrument:
         self.Y = np.log(1 + 10 * np.abs(X))
         #self.Y = np.abs(X) + EPS
         if only_self_sim:
-            self.self_sim = np.dot(np.transpose(self.Y), self.Y)
-            onset_lim = len(self.self_sim)
-            for i in range(len(self.self_sim[0])):
-                if self.self_sim[0][i] < 60:
+            normalized_Y = self.normalize(self.Y)
+            self_sim = np.dot(np.transpose(normalized_Y), normalized_Y)
+            onset_lim = len(self_sim[0])
+            self.is_cropped = False
+            for i in range(len(self_sim[0])):
+                if self_sim[0][i] < 0.4:
                     onset_lim = i
+                    self.is_cropped = True
                     break
             self.template = np.mean(self.Y[:, :onset_lim], axis=1)
         else:
             self.template = np.mean(self.Y, axis=1)
 
-    def z_norm(self, X, threshold=0.0001):
+    def is_cropped(self):
+        return self.is_cropped
+
+    def normalize(self, X, threshold=0.0001):
+        """
+        Normalize feature sequence with respect to the Euclidian norm.
+        """
         K, N = X.shape
         X_norm = np.zeros((K, N))
-        v = np.zeros(K, dtype=np.float64)
+        v = np.ones(K, dtype=np.float64) / np.sqrt(K)
         for n in range(N):
-            mu = np.sum(X[:, n]) / K
-            sigma = np.sqrt(np.sum((X[:, n] - mu) ** 2) / (K - 1))
-            if sigma > threshold:
-                X_norm[:, n] = (X[:, n] - mu) / sigma
+            s = np.sqrt(np.sum(X[:, n] ** 2)) + EPS
+            if s > threshold:
+                X_norm[:, n] = X[:, n] / s
             else:
                 X_norm[:, n] = v
         return X_norm + EPS

drum_transcription/main.py
@@ -1,43 +1,61 @@
 import numpy as np
 from reader import read_data
+import matplotlib.pyplot as plt
 
-nmf_types = ["NMFD", "NMF"]
-#nmf_types = ["NMF"]
-activation_inits = ["random", "uniform"]
-fixW_options = ["adaptive", "semi", "fixed"]
-#fixW_options = ["semi", "fixed"]
+#nmf_types = ["NMFD", "NMF"]
+nmf_types = ["NMF"]
+#activation_inits = ["random", "uniform"]
+activation_inits = ["uniform"]
+#fixW_options = ["adaptive", "semi", "fixed"]
+fixW_options = ["fixed"]
 window_sizes = [512]
+addedCompWs = range(10)
 
 params = {}
-for nmf_type in nmf_types:
-    for fixW_option in fixW_options:
-        for window_size in window_sizes:
-            for activation_init in activation_inits:
-                print("-" * 30)
-                params["nmf_type"] = nmf_type
-                params["fixW"] = fixW_option
+F = []
+for addedCompW in addedCompWs:
+    params["addedCompW"] = addedCompW
+    for nmf_type in nmf_types:
+        params["nmf_type"] = nmf_type
+        for fixW_option in fixW_options:
+            params["fixW"] = fixW_option
+            for window_size in window_sizes:
                 params["window"] = window_size
                 params["hop"] = int(window_size/2)
-                params["initH"] = activation_init
+                for activation_init in activation_inits:
+                    params["initH"] = activation_init
+                    print("-" * 30)
 
-                print(params)
+                    print(params)
 
-                data_folder = r'/Users/juliavaghy/Desktop/0--synth'
-                samples = read_data(data_folder, params)
-                #print(f"\nIncluded samples: {samples}\n")
+                    data_folder = r'/Users/juliavaghy/Desktop/0--synth'
+                    samples = read_data(data_folder, params)
+                    #print(f"\nIncluded samples: {samples}\n")
 
-                precision = np.zeros(len(samples))
-                recall = np.zeros(len(samples))
-                f_measure = np.zeros(len(samples))
-                for idx in range(len(samples)):
-                    precision[idx], recall[idx], f_measure[idx] = samples[idx].evaluate()
+                    precision = np.zeros(len(samples))
+                    recall = np.zeros(len(samples))
+                    f_measure = np.zeros(len(samples))
+                    for idx in range(len(samples)):
+                        precision[idx], recall[idx], f_measure[idx] = samples[idx].evaluate()
+                    
+                    F.append(np.mean(f_measure))
 
-                print(f"\nF={round(np.mean(f_measure), 2)}±{round(np.std(f_measure), 2)}")
-                print(f"P={round(np.mean(precision), 2)}±{round(np.std(precision), 2)}")
-                print(f"R={round(np.mean(recall), 2)}±{round(np.std(recall), 2)}\n")
+                    print(f"\nF={round(np.mean(f_measure), 2)}±{round(np.std(f_measure), 2)}")
+                    print(f"P={round(np.mean(precision), 2)}±{round(np.std(precision), 2)}")
+                    print(f"R={round(np.mean(recall), 2)}±{round(np.std(recall), 2)}\n")
 
 
-                for idx in range(len(samples)):
-                    #if abs(np.mean(f_measure) - f_measure[idx]) >= np.std(f_measure):
-                    print(f"Sample {samples[idx].dir}", end='\t')
-                    print(f"F={round(f_measure[idx], 2)}\tP={round(precision[idx], 2)}\tR={round(recall[idx], 2)}")
+                    for idx in range(len(samples)):
+                        #if abs(np.mean(f_measure) - f_measure[idx]) >= np.std(f_measure):
+                        n_instruments = len(samples[idx].instrument_codes)
+                        n_cropped = samples[idx].nmf_labels.n_cropped
+                        #print(f"Sample {samples[idx].dir}", end='\t')
+                        if f_measure[idx] > 0.9:
+                            print(f"{n_cropped} / {n_instruments}", end='\t')
+                            print(f"F={round(f_measure[idx], 2)}\tP={round(precision[idx], 2)}\tR={round(recall[idx], 2)}", end='\t')
+                            print(f"Sam {samples[idx].dir}")
+
+f, ax = plt.subplots(1)
+ax.plot(addedCompWs, F)
+ax.set_ylim(ymin=0, ymax=1)
+plt.show()